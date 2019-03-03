15d6027e6a7a989ea0062b02d8467ac6e04df56a
Mabel Villalba <mabelvj@gmail.com>
2019-03-04 00:44:50 +0100

    added url and entry_tex  field to django model

--- mysite/archive/models.py
+++ mysite/archive/models.py
@@ -5,6 +5,8 @@ class News(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     entry_type = models.CharField(max_length=200)
+    entry_text = models.TextField()
+    url = models.CharField(max_length=200)
     published_date = models.DateTimeField(
         blank=True, null=True)
 
