ff1e050b3e881da5e2288d85f2fb31f56a65c785
Mabel Villalba <mabelvj@gmail.com>
2019-03-10 21:36:25 +0100

    added pipeline to process dates

--- mysite/archive/views.py
+++ mysite/archive/views.py
@@ -3,6 +3,6 @@ from .models import New
 
 
 def news_list(request):
-    context = {'new': New.objects.order_by('entry_type', 'date')}
-
-    return render(request, 'archive/news_list.html', context)
+    news = New.objects.order_by('entry_type', 'date')
+    # .filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'archive/news_list.html', {'news': news})
--- nytimes/pipelines.py
+++ nytimes/pipelines.py
@@ -4,9 +4,22 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
+import datetime
+
+
+def process_date(d):
+    formats = ['%b. %d, %Y', '%A, %b. %d, %Y', '%A, %B %d, %Y', '%B %d, %Y']
+    for fmt in formats:
+        try:
+            return datetime.datetime.strptime(d,
+                                              fmt).strftime('%Y-%m-%d')
+        except:
+            pass
+    raise ValueError('no valid date format found: %s' % d)
 
 
 class NewsPipeline(object):
     def process_item(self, item, spider):
-        item.save()
+        item['date'] = process_date(item['date'])
+        item.save(commit=True)
         return item
