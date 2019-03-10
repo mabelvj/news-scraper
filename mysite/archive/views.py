8b6379e926fe0357f2ac1868e015350fd248d127
Mabel Villalba <mabelvj@gmail.com>
2019-03-10 22:25:34 +0100

    added pagination

--- mysite/archive/views.py
+++ mysite/archive/views.py
@@ -1,8 +1,13 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from .models import New
 
 
 def news_list(request):
-    news = New.objects.order_by('entry_type', 'date')
+    news_list_ = New.objects.order_by('entry_type', 'date')
     # .filter(published_date__lte=timezone.now()).order_by('published_date')
+    paginator = Paginator(news_list_, 25)  # Show 25 contacts per page
+
+    page = request.GET.get('page')
+    news = paginator.get_page(page)
     return render(request, 'archive/news_list.html', {'news': news})
